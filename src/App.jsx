b872316cf10b14c10d02ed46b4b594e893ad0297
shahabas <shazz4568@gmail.com>
2025-04-28 09:26:51 +0530

    project completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,42 @@
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import Home from './Pages/Home';
+import { lazy, Suspense, useEffect, useState } from 'react';
 import Header from './Components/Header';
-import Cart from './Pages/Cart';
-import Login from './Pages/Login';
-import Register from './Pages/Register';
-import Adminscreen from './Components/Adminscreen';
-import Order from './Components/Order';
-import Drink from './Components/Drink';
-import { useEffect, useState } from 'react';
+import './App.css';
+const Home = lazy(() => import('./Pages/Home'));
+const Cart = lazy(() => import('./Pages/Cart'));
+const Login = lazy(() => import('./Pages/Login'));
+const Register = lazy(() => import('./Pages/Register'));
+const Adminscreen = lazy(() => import('./Components/Adminscreen'));
+const Order = lazy(() => import('./Components/Order'));
+const Drink = lazy(() => import('./Components/Drink'));
 
 function App() {
+  const [user, setUser] = useState(null);
 
-    const [user, setUser] = useState(null);
-  
-    useEffect(() => {
-      const storedUser = localStorage.getItem('user');
-      if (storedUser) {
-        setUser(JSON.parse(storedUser));
-      }
-    }, []);
+  useEffect(() => {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
   return (
     <>
-      <Header user={user}/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/admin/*" element={<Adminscreen />} />
-        <Route path="/order" element={<Order />} />
-        <Route path="/drink" element={<Drink />} />
-      </Routes>
-
-      
+      <Header user={user} />
+      {/* Suspense = Loading Spinner or fallback until component load */}
+      <Suspense fallback={<h2>Loading...</h2>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/admin/*" element={<Adminscreen />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/drink" element={<Drink />} />
+          <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+        </Routes>
+      </Suspense>
     </>
-
-    
   );
 }
 
