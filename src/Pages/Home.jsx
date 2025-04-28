171129b661acd27f3dee28488c3c293c58f5a42e
shabas123 <shazz4568@gmail.com>
2025-04-28 10:47:51 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -30,7 +30,7 @@ export default function Home() {
   return (
     <>
 
-     <div className="container-fluid d-flex align-items-center justify-content-center min-vh-100 bg">
+     <div className="container-fluid d-flex align-items-center justify-content-center min-vh-100 bg" style={{marginTop:"80px"}}>
         <div className="row align-items-center">
           <div className="col-md-6 " style={{ padding: "30px" }}>
         {  loggedInUser ?  <h2 className="fw-bold text-danger " > Welcome {loggedInUser} </h2>
@@ -63,16 +63,16 @@ export default function Home() {
 }     {loggedInUser && <a href="#section1">
         <div className='row bg'>
 
-//         <div className='posters'>
-//           <img className='poster' alt='poster' src='https://kitchen.sayidaty.net/uploads/small/8f/8f584bb7cc5256c5ac3fcb4571513616_w750_h500.jpg' />
-//           <img className='poster' alt='poster' src='https://ichef.bbci.co.uk/food/ic/food_16x9_1600/recipes/chinese_chicken_curry_90700_16x9.jpg' />
-//           <img className='poster' alt='poster' src='https://img.onmanorama.com/content/dam/mm/en/food/recipe/images/2021/2/14/alfaham.jpg' />
-//           <img className='poster' alt='poster' src='https://t4.ftcdn.net/jpg/03/24/50/53/360_F_324505357_SWfRhx7862SwygH4jHJAbXPdgKA3nxm6.jpg' />
-//           <img className='poster' alt='poster' src='https://images.deliveryhero.io/image/fd-pk/LH/r7bw-hero.jpg' />
-//           <img className='poster' alt='poster' src='https://static.vecteezy.com/system/resources/previews/024/678/534/non_2x/shawarma-with-chicken-and-vegetables-arabic-style-generative-ai-free-photo.jpeg' />
-//           <img className='poster' alt='poster' src='https://img.freepik.com/premium-photo/butter-naan-panner-butter-masala-indian-dish_651966-38.jpg' />
-//         </div>
-//       </div>
+       <div className='posters'>
+          <img className='poster' alt='poster' src='https://kitchen.sayidaty.net/uploads/small/8f/8f584bb7cc5256c5ac3fcb4571513616_w750_h500.jpg' />
+          <img className='poster' alt='poster' src='https://ichef.bbci.co.uk/food/ic/food_16x9_1600/recipes/chinese_chicken_curry_90700_16x9.jpg' />
+          <img className='poster' alt='poster' src='https://img.onmanorama.com/content/dam/mm/en/food/recipe/images/2021/2/14/alfaham.jpg' />
+          <img className='poster' alt='poster' src='https://t4.ftcdn.net/jpg/03/24/50/53/360_F_324505357_SWfRhx7862SwygH4jHJAbXPdgKA3nxm6.jpg' />
+           <img className='poster' alt='poster' src='https://images.deliveryhero.io/image/fd-pk/LH/r7bw-hero.jpg' />
+           <img className='poster' alt='poster' src='https://static.vecteezy.com/system/resources/previews/024/678/534/non_2x/shawarma-with-chicken-and-vegetables-arabic-style-generative-ai-free-photo.jpeg' />
+          <img className='poster' alt='poster' src='https://img.freepik.com/premium-photo/butter-naan-panner-butter-masala-indian-dish_651966-38.jpg' />
+        </div>
+      </div>
       </a>}
      {loggedInUser && <a href="/drink">
 //       <div className="row bg">
@@ -92,7 +92,7 @@ export default function Home() {
 }     {loggedInUser && <div className="row justify-content-center bg"  >
         {
           items.map((item => {
-            return <div className="col-md-4" key={item._id}>
+            return <div className="col-md-4 col-12" key={item._id} style={{paddingLeft:"20px"}}>
               <div>
                 <Item item={item} />
               </div>
