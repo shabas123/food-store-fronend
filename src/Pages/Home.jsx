c4b826297f0e3e6a42aa9c6d94eb95a521e452ac
shabas123 <shazz4568@gmail.com>
2025-05-05 10:00:05 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -64,30 +64,30 @@ export default function Home() {
 }     {loggedInUser && <a href="#section1">
         <div className='row bg'>
 
-//         <div className='posters'>
-//           <img className='poster' alt='poster' src='https://kitchen.sayidaty.net/uploads/small/8f/8f584bb7cc5256c5ac3fcb4571513616_w750_h500.jpg' />
-//           <img className='poster' alt='poster' src='https://ichef.bbci.co.uk/food/ic/food_16x9_1600/recipes/chinese_chicken_curry_90700_16x9.jpg' />
-//           <img className='poster' alt='poster' src='https://img.onmanorama.com/content/dam/mm/en/food/recipe/images/2021/2/14/alfaham.jpg' />
-//           <img className='poster' alt='poster' src='https://t4.ftcdn.net/jpg/03/24/50/53/360_F_324505357_SWfRhx7862SwygH4jHJAbXPdgKA3nxm6.jpg' />
-//           <img className='poster' alt='poster' src='https://images.deliveryhero.io/image/fd-pk/LH/r7bw-hero.jpg' />
-//           <img className='poster' alt='poster' src='https://static.vecteezy.com/system/resources/previews/024/678/534/non_2x/shawarma-with-chicken-and-vegetables-arabic-style-generative-ai-free-photo.jpeg' />
-//           <img className='poster' alt='poster' src='https://img.freepik.com/premium-photo/butter-naan-panner-butter-masala-indian-dish_651966-38.jpg' />
-//         </div>
-//       </div>
+        <div className='posters'>
+           <img className='poster' alt='poster' src='https://kitchen.sayidaty.net/uploads/small/8f/8f584bb7cc5256c5ac3fcb4571513616_w750_h500.jpg' />
+           <img className='poster' alt='poster' src='https://ichef.bbci.co.uk/food/ic/food_16x9_1600/recipes/chinese_chicken_curry_90700_16x9.jpg' />
+           <img className='poster' alt='poster' src='https://img.onmanorama.com/content/dam/mm/en/food/recipe/images/2021/2/14/alfaham.jpg' />
+           <img className='poster' alt='poster' src='https://t4.ftcdn.net/jpg/03/24/50/53/360_F_324505357_SWfRhx7862SwygH4jHJAbXPdgKA3nxm6.jpg' />
+           <img className='poster' alt='poster' src='https://images.deliveryhero.io/image/fd-pk/LH/r7bw-hero.jpg' />
+           <img className='poster' alt='poster' src='https://static.vecteezy.com/system/resources/previews/024/678/534/non_2x/shawarma-with-chicken-and-vegetables-arabic-style-generative-ai-free-photo.jpeg' />
+           <img className='poster' alt='poster' src='https://img.freepik.com/premium-photo/butter-naan-panner-butter-masala-indian-dish_651966-38.jpg' />
+         </div>
+       </div>
       </a>}
      {loggedInUser && <a href="/drink">
-//       <div className="row bg">
-//         <div className="posters">
-//           <img className='poster' alt='poster' src='https://rare-gallery.com/uploads/posts/841890-Drinks-Lime-Mojito-Black-background-Two-Highball-glass.jpg' />
-//           <img className='poster' alt='poster' src='https://wallpapercave.com/wp/wp7947935.jpg' />
-//           <img className='poster' alt='poster' src='https://images8.alphacoders.com/397/thumb-1920-397307.jpg' />
-//           <img className='poster' alt='poster' src='https://wallpapercave.com/wp/wp3631978.jpg' />
-//           <img className='poster' alt='poster' src='https://images2.alphacoders.com/876/876244.jpg' />
-//           <img className='poster' alt='poster' src='https://pod2.brvn.vn/media/2023/09/7up1.webp' />
+       <div className="row bg">
+         <div className="posters">
+           <img className='poster' alt='poster' src='https://rare-gallery.com/uploads/posts/841890-Drinks-Lime-Mojito-Black-background-Two-Highball-glass.jpg' />
+           <img className='poster' alt='poster' src='https://wallpapercave.com/wp/wp7947935.jpg' />
+           <img className='poster' alt='poster' src='https://images8.alphacoders.com/397/thumb-1920-397307.jpg' />
+           <img className='poster' alt='poster' src='https://wallpapercave.com/wp/wp3631978.jpg' />
+           <img className='poster' alt='poster' src='https://images2.alphacoders.com/876/876244.jpg' />
+           <img className='poster' alt='poster' src='https://pod2.brvn.vn/media/2023/09/7up1.webp' />
 
 
-//         </div>
-//       </div>
+         </div>
+       </div>
       </a>}
 
 { loggedInUser &&     <h2 id="section1" className='text-center mt-4'>  Today's Special </h2>
